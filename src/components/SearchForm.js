def93c01472f46b9d14cd51e94293bb64493fdf8
Vincent Sanders <vincesanders@hotmail.com>
2019-12-20 11:10:33 -0600

    Added functionality to search through characters. This means I have satisfied MVP!

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 
 const SearchFormContainer = styled.div`
   text-align: center;
 `
 
-export default function SearchForm() {
+export default function SearchForm(props) {
   const [query, setQuery] = useState('');
   const handleInputChange = e => {
     setQuery(e.target.value);
   }
+
+  useEffect(() => {
+    const characters = props.characterData.filter(char => {
+      return char.name.toLowerCase().includes(query.toLowerCase());
+    });
+    props.setActiveCharacterData(characters);
+  },[query])
  
   return (
     <SearchFormContainer>
