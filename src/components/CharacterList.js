aba03c9eba91ab62e6cbf5522d159a2ea88a4c5f
Vincent Sanders <vincesanders@hotmail.com>
2019-12-20 10:39:54 -0600

    Added axios call. Added map function to display character list. Added styling and markup to CharacterCard.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characterData, setCharacterData] = useState([]);
+  const [activeCharacterData, setActiveCharacterData] = useState([]); //so I don't have to make another axios 
+                                                                      //call to search through the data
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    // axios.get('https://rickandmortyapi.com/api/character/')
+    //   .then(res => {
+    //     console.log(res);
+    //     setCharacterData(res.data.results);
+    //     setActiveCharacterData(res.data.results)
+    //   })
+    //   .catch(err => console.log(err));
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <CharacterCard name='This is a test' status='Alive' species='human' />
+      {useEffect(() => {
+        characterData.map(char => (
+          <CharacterCard name={char.name} status={char.status} species={char.species} />
+        ));
+      },[activeCharacterData])}    
     </section>
   );
 }
