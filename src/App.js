2aad84106c4308c4f983a96a15ef19755160d444
Vincent Sanders <vincesanders@hotmail.com>
2019-12-20 10:21:30 -0600

    Added WelcomePage to App within a Route within a Switch. Imported styled-components into WelcomePage and styled the main image.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Route, link, Switch } from 'react-router-dom';
 import Header from "./components/Header.js";
+import WelcomePage from './components/WelcomePage';
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Switch>
+        <Route path='/'>
+          <WelcomePage />
+        </Route>
+      </Switch>
     </main>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,12 +1,19 @@
 import React from "react";
+import styled from 'styled-components';
+
+
+const MainImg = styled.img`
+  display: block;
+  border-radius: 100%;
+  margin: 0 auto;
+`
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
-        <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
+      <h1>Welcome to the ultimate fan site!</h1>
+        <MainImg
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
